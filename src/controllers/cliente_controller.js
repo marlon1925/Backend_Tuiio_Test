e86fac49e06113f5d19f3ff425ef99aca6017738
Jeanpy171 <jeanpyfuentes67@gmail.com>
2023-11-23 10:28:03 -0500

    test sendCode

--- src/controllers/cliente_controller.js
+++ src/controllers/cliente_controller.js
@@ -4,20 +4,63 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const validateType = (phoneMail) => {
+  if (/@/.test(phoneMail)) {
+    return "email";
+  } else {
+    return "phone";
+  }
+};
+
 const sendCodeClient = async (req, res) => {
+  console.log("\n________________________________________");
+  console.log("\nINICIA SERVICIO DE ENVIO DE CÓDIGO OTP");
+  console.log("\n________________________________________");
+  console.log("ESTO TIENE REQ: ", req.body);
   try {
-    if (Object.values(req.body).includes("")) {
-      return res
-        .status(400)
-        .json({ msg: "Lo sentimos, los campos deben estar llenos" });
+    // Verificar que todos los campos requeridos estén presentes
+    const requiredFields = ["tipo", "canal", "phoneMail"];
+    const missingFields = requiredFields.filter(
+      (field) => !(field in req.body)
+    );
+
+    if (missingFields.length > 0) {
+      return res.status(401).json({
+        result: false,
+        messages: [
+          `Los campos siguientes son requeridos: ${missingFields.join(", ")}`,
+        ],
+      });
     }
 
-    // Devolver una variable quemada de verdadero cuando el registro sea exitoso
-    res.status(200).json({
-      result: true,
-      data: { idEnte: 0, idProce: 0, mode: 0 },
-      messages: [],
-    });
+    // Verificar que el campo 'code' no esté vacío
+    if (!req.body.phoneMail || req.body.phoneMail.trim() === "") {
+      return res.status(402).json({
+        result: false,
+        messages: ["No existe un medio para enviar el código"],
+      });
+    }
+
+    // Lógica adicional de validación si es necesario
+    if (validateType(req.body.phoneMail) === "email") {
+      // ES MAIL
+      res.status(200).json({
+        result: true,
+        data: { idEnte: 0, idProce: 0, mode: 0 },
+        messages: [
+          "El código de verificación ha sido enviado a tu correo electrónico",
+        ],
+      });
+    } else {
+      // ES PHONE
+      res.status(200).json({
+        result: true,
+        data: { idEnte: 0, idProce: 0, mode: 0 },
+        messages: [
+          "El código de verificación ha sido enviado a tu número telefónico",
+        ],
+      });
+    }
   } catch (error) {
     console.log(error);
     // Manejar el error y devolver una variable quemada de falso en caso de error
@@ -47,7 +90,7 @@ const validationCode = async (req, res) => {
 
     // Verificar que el campo 'code' no esté vacío
     if (!req.body.code || req.body.code.trim() === "") {
-      return res.status(401).json({
+      return res.status(402).json({
         result: false,
         messages: ["El campo 'code' no puede estar vacío"],
       });
@@ -63,10 +106,10 @@ const validationCode = async (req, res) => {
       });
     } else {
       // Código inválido
-      res.status(401).json({
+      res.status(403).json({
         result: false,
         data: { idEnte: 0, idProce: 0, mode: 0 },
-        messages: ["Código incorrecto"],
+        messages: ["Código no válido"],
       });
     }
   } catch (error) {
