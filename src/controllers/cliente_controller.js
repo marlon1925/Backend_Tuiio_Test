9f0e6e1f668e4d23d6a518c4a01073b7ffe0d38f
Jeanpy171 <jeanpyfuentes67@gmail.com>
2023-11-30 08:51:22 -0500

    getPersonal data fix

--- src/controllers/cliente_controller.js
+++ src/controllers/cliente_controller.js
@@ -301,29 +301,37 @@ const getPersonalInfo = async (req, res) => {
       let infoCurp = getInfoCurp(req.body.curp);
 
       if (infoCurp) {
-        // res.status(200).json({
-        //   result: true,
-        //   data: { ...infoCurp },
-        //   messages: ["Información obtenida con éxito"],
-        // });
         res.status(200).json({
           result: true,
           data: {
-            code: "OK0001",
-            message: "PETICION EXITOSA",
-            curp: "SASJ010616HDFNNNA3",
-            name: "JUAN JESUS",
-            lastName: "SANTOS",
-            secondLastName: "SANCHEZ",
-            sex: "H",
+            ...infoCurp,
             birthDate: "16/06/2001",
             birthPlace: "DF",
             nationality: "MEX",
             errorMessage: null,
+            code: "OK0001",
             errores: null,
           },
           messages: ["Información obtenida con éxito"],
         });
+        // res.status(200).json({
+        //   result: true,
+        //   data: {
+        //     code: "OK0001",
+        //     message: "PETICION EXITOSA",
+        //     curp: "SASJ010616HDFNNNA3",
+        //     name: "JUAN JESUS",
+        //     lastName: "SANTOS",
+        //     secondLastName: "SANCHEZ",
+        //     sex: "H",
+        //     birthDate: "16/06/2001",
+        //     birthPlace: "DF",
+        //     nationality: "MEX",
+        //     errorMessage: null,
+        //     errores: null,
+        //   },
+        //   messages: ["Información obtenida con éxito"],
+        // });
       } else {
         res.status(403).json({
           result: true,
