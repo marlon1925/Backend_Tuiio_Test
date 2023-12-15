ad9e1e8a233ab9645842c753415a1215631f1480
Adrian Chicaiza <74798975+AdrianChicaiza@users.noreply.github.com>
2023-12-14 22:06:18 -0500

    add url

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -18,16 +18,20 @@ import {
 const router = Router();
 
 // router.post("/security/client/sendCode", bodyParser.json(), sendCodeClient);
-router.post("/security/login", bodyParser.json(), LoginAdmin);
-router.post("/loan/elavonPayments", bodyParser.json(), PagosElavon);
-router.post("/client/parameters", bodyParser.json(), ObtenerParametrosPrestamo);
-router.post("/loan/info", bodyParser.json(), ObtenerInformacionPrestamo);
-router.post("/security/password", bodyParser.json(), DefinirContrasena);
-router.post("/security/onboard", bodyParser.json(), ValidarEnrolamiento);
-router.post("/mail/valid", bodyParser.json(), ReenvioEnrolamiento);
-router.post("/security/logout", bodyParser.json(), LogoutAdmin);
-router.post("/loan/info/prestamo", bodyParser.json(), Prestamo);
-router.get("/security/client/parameters", bodyParser.json(), ObtenerParametros);
-router.put("/client/changePhone", bodyParser.json(), CambiarTelefono);
+router.post("security/login", bodyParser.json(), LoginAdmin);
+router.post("loan/elavonPayments", bodyParser.json(), PagosElavon);
+router.post(
+  "security/client/parameters",
+  bodyParser.json(),
+  ObtenerParametrosPrestamo
+);
+router.post("loan/info", bodyParser.json(), ObtenerInformacionPrestamo); //igual
+router.post("security/password", bodyParser.json(), DefinirContrasena);
+router.post("security/onboard", bodyParser.json(), ValidarEnrolamiento);
+router.post("security/mail/valid", bodyParser.json(), ReenvioEnrolamiento);
+router.post("security/logout", bodyParser.json(), LogoutAdmin);
+router.post("loan/info", bodyParser.json(), Prestamo); //igual
+router.get("security/client/parameters", bodyParser.json(), ObtenerParametros);
+router.put("security/client/changePhone", bodyParser.json(), CambiarTelefono);
 
 export default router;
